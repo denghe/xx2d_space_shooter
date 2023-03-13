499e1b95fee7c6e6cb44bb3fbe0cdcdfe0930c85
denghe <denghe@hotmail.com>
2023-03-13 23:21:33 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "main.h"
+
+int GameLooper::Init() {
+	fnt = xx::engine.LoadBMFont("res/font/coderscrux.fnt"sv);
+	fpsViewer.Init(fnt);
+
+	//scene = xx::Make<MainMenu::Scene>();
+	//scene->Init(this);
+
+	return 0;
+}
+
+int GameLooper::Update() {
+	if (xx::engine.Pressed(xx::KbdKeys::Escape)) return 1;
+
+	//int r = scene->Update();
+
+	fpsViewer.Update();
+	return 0;
+}
+
+int main() {
+	return GameLooper{}.Run();
+}
---
+++ src/main.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "xx2d.h"
+#include "xx2d_fps_viewer.h"
+
+struct GameLooper;
+struct SceneBase {
+	GameLooper* looper{};
+	virtual void Init(GameLooper*) = 0;
+	virtual int Update() = 0;
+	virtual ~SceneBase() {};
+};
+
+struct GameLooper : xx::GameLooperBase {
+	xx::BMFont fnt;
+	xx::FpsViewer fpsViewer;
+	double timePool{};
+
+	xx::Shared<SceneBase> scene;
+
+	int Init() override;
+	int Update() override;
+};
